b368c235c1a7344780b300f63962bb292928d820
MahatKC <mkarvat@gmail.com>
2022-03-28 10:39:33 -0300

    function created

--- sintatico.py
+++ sintatico.py
@@ -1,20 +1,22 @@
 import token
 
+def read_token_file(token_list):
+    f = open("tokens.txt")
+
+    for line in f:
+        token_end=2
+        for char in line[2:-2]:
+            if char=="'":
+                break
+            token_end+=1
+
+        token = line[2:token_end]
+        value = line[token_end+4:-3]
+
+        token_list.append([token,value])
 
 token_list = []
-f = open("tokens.txt")
-
-for line in f:
-    token_end=2
-    for char in line[2:-2]:
-        if char=="'":
-            break
-        token_end+=1
-
-    token = line[2:token_end]
-    value = line[token_end+4:-3]
-
-    token_list.append([token,value])
+read_token_file(token_list)
 
 for token_value_pair in token_list:
     pass
